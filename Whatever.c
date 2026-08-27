f3ab7501e3ba67cdbdd6c181ef4d3b979b88ccf8
Abhishek D Bhat <abhishekbhat0p@gmail.com>
2026-08-27 19:39:15 +0530

    Refactor array manipulation functions for consistency

--- Whatever.c
+++ Whatever.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
-void insert(int pos, int val, int arr[], int n){
-    for(int i=n; i>pos;i--){
-        arr[i]=arr[i-1];
+void insert(int pos, int val, int arr[], int n) {
+    for (int i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
     }
-    arr[pos]=val;
+    arr[pos] = val;
+}
+
+void delete_at(int pos, int arr[], int n) {
+    for (int i = pos - 1; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+}
+void search(int val, int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == val) {
+            printf(" Found at %d\n", i);
+            break;
+        }
+    }
 }
 int main() {
     int i;
@@ -14,12 +28,14 @@ int main() {
         3,
         4
     };
-    insert(3,5,arr,6);
+    insert(3, 5, arr, 6);
     int n = sizeof(arr) / sizeof(arr[0]);
+    delete_at(4, arr, 6);
+    search(3, arr, 6);
     for (i = 0; i < n; i++) {
-        printf("%d \n", arr[i]);
+        printf("%d ", arr[i]);
     }
-    
+
     return 0;
 
 }
